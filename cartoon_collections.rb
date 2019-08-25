3b38b3237cadb1c38f116b367f96c2398769402a
Catherine Clause <cnclause123@gmail.com>
2019-08-25 18:13:10 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -22,9 +22,9 @@ end
 def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"] 
   i = 0 
-  if array.find {|type| cheese_types.include?(type)}
-    return
-  else return nil
+  while i < array.length 
+  array.find {|type| cheese_types.include?(type)}
   i += 1 
+end
 end
 
